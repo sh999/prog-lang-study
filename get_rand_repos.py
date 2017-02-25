434925aaeb3d26d2df173e30d4b47aedf4482083
mzhang0 <mzhang0125@gmail.com>
2017-02-25 17:49:31 -0500

    Added support for authenticated API calls.

--- get_rand_repos.py
+++ get_rand_repos.py
@@ -1,30 +1,34 @@
-import requests, random, csv, sys
+import requests, random, csv, sys, time
+from secrets import gh_auth_token
 
 repos = {}
 base_url = 'https://api.github.com/repositories?since='
-auth_key = ''
+token_str = "token " + gh_auth_token
+headers = {'user-agent': 'prog-lang', 'Authorization': token_str}
 
 def dump_csv():
 	with open('repo_info.csv', 'w') as f:
 		csvwriter = csv.writer(f)
 		for k, v in repos.items():
-			csvwriter.writerow(k, v)
+			csvwriter.writerow((k, v))
 
 def request_loop():
 	counter = 0
 	while counter <= 100000:
 		
 		rand_id = random.randrange(1, 82605000)
-		resp = requests.get(base_url + str(rand_id))
+		url = base_url + str(rand_id)
+		resp = requests.get(url, headers=headers)
 
 		if resp.status_code != 403:
 			j_resp = resp.json()
 		else:
-			rate_resp = requests.get('https://api.github.com/rate_limit').json()
+			rate_resp = requests.get('https://api.github.com/rate_limit',
+				headers=headers).json()
 			print(rate_resp)
-			dump_csv()
-			print("\nExiting due to rate limit.\nCounter: " + str(counter))
-			sys.exit()
+			print("Sleeping due to rate limit.\n\tCounter: " + str(counter))
+			time.sleep(3660)
+			
 
 		for entry in j_resp:
 			if entry['private'] == False and entry['fork'] == False:
