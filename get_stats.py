e32ef142d8642aae0801d6e27f8c92b30f2f7a07
sh999 <sh999gh@gmail.com>
2017-03-28 04:00:30 -0400

    Allow command line args to run build data or get stats

--- get_stats.py
+++ get_stats.py
@@ -7,25 +7,28 @@ import json
 import pickle
 import re
 import operator
+import sys
 from pprint import pprint
 
-def make_pickle():
-	file_string = "repo_info_default.json"
+def make_pickle(file_string):
+	# file_string = "repo_info_default.json"
 	# file_string = "test.json"		
 	# file_string = "repo_info_minimal.json"
 	with open(file_string, 'r',encoding="utf8") as jf:
 	    data = json.load(jf)
-	outfile = open("repo_out_all.pickle", "wb")
+	outfile = open(file_string+".pickle", "wb")
 	pickle.dump(data, outfile)
 
-def get_stats():
-	data = open("repo_out.pickle","rb")
+def get_stats(pickle_filename):
+	# data = open("repo_out.pickle","rb")
+	print("Loading pickle")
+	data = open(pickle_filename,"rb")
 	data = pickle.load(data)
 	lang_count = {}
 	lang_count_lowcommits = {}
-	commits_out = open("commits_out.csv","w")
+	commits_out = open(pickle_filename+"commits_out.csv","w")
 	commits_out.write("full_name,language,commits_count\n")
-	langs_out = open("langs_out.csv","w")
+	langs_out = open(pickle_filename+"langs_out.csv","w")
 	for repo in data:
 		if repo["language"] not in lang_count:
 			lang_count[repo["language"]]=0
@@ -44,5 +47,9 @@ def get_stats():
 	# sorted_lang_lowcommits = sorted(lang_count_lowcommits.items(), key=operator.itemgetter(1))
 	# pprint(sorted_lang_lowcommits)
 
-make_pickle()
-# get_stats()
+json_file = sys.argv[2]
+print(sys.argv)
+if sys.argv[1] == "pickle":
+	make_pickle(json_file)
+elif sys.argv[1] == "stats":
+	get_stats(json_file)
