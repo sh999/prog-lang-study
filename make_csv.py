12e8dc8654de45fa0b542d00aea64dfed37cb742
sh999 <sh999gh@gmail.com>
2017-03-28 05:49:49 -0400

    Convert list of commit timestamps to nth month

--- make_csv.py
+++ make_csv.py
@@ -8,6 +8,16 @@ Input:  pickle file based on json file (built from get_stats.py)
 '''
 import sys, json, pickle
 
+def timestamp_to_nth_month(commit_time,first_commit_time):
+	'''
+		Return the nth month in the time interval of commits
+		Helper function to process list of commits
+	'''
+	diff_years = int(commit_time[:4])-int(first_commit_time[:4])-1
+	diff_months = int(commit_time[5:7])+(12-int(first_commit_time[5:7]))
+	diff_months = diff_years*12+diff_months
+	return diff_months
+
 def get_commit_months(commit_list):
 	'''
 		Commit list has timestamps. 
@@ -22,8 +32,13 @@ def get_commit_months(commit_list):
 	years = int(last_commit_time[:4])-int(first_commit_time[:4])-1
 	months = int(last_commit_time[5:7])+(12-int(first_commit_time[5:7]))
 	months = years*12+months
-	
+
+	commit_time = commit_list[4]
+	print(timestamp_to_nth_month(commit_time,first_commit_time))
+
 	print("\n")
+	commit_list = [timestamp_to_nth_month(commit,first_commit_time) for commit in commit_list]
+	print(commit_list)
 	return commit_list
 
 
