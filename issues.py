b6e0de8e913a2735b9f1e8bd93dff5f0555f7f82
sh999 <sh999gh@gmail.com>
2019-03-06 15:26:26 -0500

    Add issues.py, which fetches issues data for repos

---
+++ issues.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+# USAGE: issues.py <repo url> [--username <username> --password <password>]
+# NOTE: Requires the GitHub Python library (`pip3 install PyGitHub`).
+
+from argparse import ArgumentParser
+from github import Github
+
+
+def get_issues(repo_name):
+    print('Retreiving issue data from "{0}"...'.format(repo_name))
+    repository = github.get_repo(repo_name)
+    issues = [issue for issue in repository.get_issues()]
+    open = [issue for issue in issues if issue.state == 'open']
+    closed = [issue for issue in issues if issue.state == 'closed']
+
+    print('{0} issues found. ({1} open, {2} closed)'.format(len(issues), len(open), len(closed)))
+    summary = '{0} issues found. ({1} open, {2} closed)'.format(len(issues), len(open), len(closed))
+    for issue in issues:
+        print('#{0:04d} ({1}):\t{2}'.format(issue.number, issue.state, issue.title))
+    return summary
+
+  
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Analyze GitHub repository issues.')
+    parser.add_argument('-u', '--username', help='The username to sign in with, if you need.')
+    parser.add_argument('-p', '--password', help='The password to sign in with, if you need.')
+    args = parser.parse_args()
+
+    github = Github(args.username, args.password)
+
+    repos = open("repo_info.csv", "r")
+    for item in repos.readlines():
+        print('Retreiving issue data from ' + item )
+        print("item : " + item)
+        repo_name = item.split(",")[1].rstrip()
+        print(repo_name)
+        summary = get_issues(repo_name)
